bb6f3f701acacbbec4a97760b9ab7f8cc4c8b314
Ajay Edupuganti <ajsmac@porsche.local>
2024-01-26 19:24:39 +0530

    My cat tool

---
+++ My Cat tool/my_cat.py
@@ -0,0 +1,45 @@
+#!/opt/anaconda3/bin/python3
+
+import sys
+
+def printlines():
+    for line in sys.stdin:
+        print(line)
+
+def printlineswithspaces():
+    count = 0
+    for line in sys.stdin:
+        stripped_line = line.strip()
+        count +=1
+        if stripped_line:
+            print(count, stripped_line)
+        else:
+            print(count,line, end='')
+
+def printlineswithoutspaces():
+    count = 0
+    for line in sys.stdin:
+        stripped_line=line.strip()
+        if stripped_line:
+            count += 1
+            print(count, stripped_line)
+        else:
+            print(stripped_line)
+
+
+files = sys.argv
+
+for i in range(1,len(sys.argv)):
+    filename = files[i]
+    if filename == '-':
+        printlines()
+    elif filename == '-n':
+        printlineswithspaces()
+    elif filename == '-b':
+        printlineswithoutspaces()
+    else:
+        content = ''
+        with open(filename,'r') as file:
+            content += file.read()
+        print(content,end='')
+
