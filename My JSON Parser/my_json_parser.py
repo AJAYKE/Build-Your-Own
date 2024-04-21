c4ecdc2568232c0504e059a24ddeca294b4c3597
AJAYKE <152738727+AJAYKE@users.noreply.github.com>
2024-04-21 16:18:34 +0530

    vercel 2.0

--- My JSON Parser/my_json_parser.py
+++ My JSON Parser/my_json_parser.py
@@ -4,29 +4,43 @@ import os
 def check_for_brackets(brackets):
     return brackets[0] == '{' and brackets[-1] == '}' 
 
+def check_string_keys_and_string_values(string_keys_and_values):
+    print(string_keys_and_values)
+    for pair in string_keys_and_values:
+        key_value = pair.split(":")
+        print(pair, key_value)
+        if not (type(key_value[0].strip()) == str and type(key_value[1].strip()) == str):
+            print(key_value[0].strip(), type(key_value[0].strip()))
+            return 0
+    return 1
+
 def check_for_json(content):
     brackets = list(content)
     if not check_for_brackets(brackets):
+        print(brackets,brackets[0], brackets[-1])
+        print("failed in brackets")
+        return 0
+    
+    string_keys_and_values = content[1:-1].split(',')
+
+    print(content, brackets, string_keys_and_values)
+
+    if not check_string_keys_and_string_values(string_keys_and_values):
+        print("failed in strings")
         return 0
     return 1
 
-if len(sys.argv) < 2:
-    print("Usage: python my_json_parser.py <directory_path>")
-    sys.exit(1)
 
-directory = sys.argv[1]
+directory = 'tests/step2'
 
-if not os.path.isdir(directory):
-    print("Error: The specified directory does not exist.")
-    sys.exit(1)
-
-files = os.listdir(sys.argv[1])
+files = os.listdir(directory)
 
 for file in files:
     file_path = os.path.join(directory, file)
     content = open(file_path,'r').read()
     try:
         res = check_for_json(content)
-    except:
+    except Exception as error:
+        print(error)
         res = 0
     print(file, res)
